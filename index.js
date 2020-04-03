8ec1cad418b5f47a66a915faf95c4dd40f73e697
Marcelo <marcelo.scientist@hotmail.com>
2020-04-03 15:03:00 -0300

    first part of the challenge done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
 const server = express()
+server.use(express.json())
+const projects = [
+  { 
+    id: "1", 
+    title: 'New project', 
+    tasks: []
+  }
+]
+server.post('/projects', (req,res,next)=>{
+  const project = req.body
+  projects.push({project})
+  console.log(projects)
+  res.json({project})
+  
+}) 
 
 server.listen(3000)
